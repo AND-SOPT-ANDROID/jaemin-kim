b91c0f84caf5d6d35ef0002fb0dd6cde63e4ceb5
Jaemin <kamja990510@gmail.com>
2024-10-07 22:31:52 +0900

    파일명 공백 제거

--- app/src/main/java/org/sopt/and/MainActivity.kt
+++ app/src/main/java/org/sopt/and/MainActivity.kt
@@ -38,7 +38,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ANDANDROIDTheme {

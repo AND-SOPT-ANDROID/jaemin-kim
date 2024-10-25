8b780d3e16674925eee8747bfc8c5b3fc50fd1da
Jaemin <kamja990510@gmail.com>
2024-10-25 20:04:15 +0900

    [fix] #3 Fix home screen ui

--- app/src/main/java/org/sopt/and/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/and/home/HomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -40,7 +41,7 @@ fun HomeScreen(
 
         Column(
             modifier = Modifier
-                .height(400.dp)
+                .fillMaxWidth()
                 .verticalScroll(scrollState)
         ) {
             HomeBannerPager()

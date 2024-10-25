54acbfa6171bf27846398a939d24b96b3edf3a11
Jaemin <kamja990510@gmail.com>
2024-10-25 19:46:43 +0900

    [refactor] #3 Extract string

--- app/src/main/java/org/sopt/and/components/Coupon.kt
+++ app/src/main/java/org/sopt/and/components/Coupon.kt
@@ -9,13 +9,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import org.sopt.and.R
 
 @Composable
 fun Coupon(modifier: Modifier = Modifier) {
     Text(
-        text = "첫 결제 시 첫 달 100원!",
+        text = stringResource(R.string.my_first_payment_text),
         textAlign = TextAlign.Center,
         color = Color.White,
         modifier = modifier
@@ -23,8 +25,8 @@ fun Coupon(modifier: Modifier = Modifier) {
             .background(
                 brush = Brush.linearGradient(
                     colors = listOf(
-                        Color(0xFF0281ED), // Start color (0281ED)
-                        Color(0xFF02B9B5)  // End color (02B9B5)
+                        Color(0xFF0281ED),
+                        Color(0xFF02B9B5)
                     )
                 ),
                 shape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp)

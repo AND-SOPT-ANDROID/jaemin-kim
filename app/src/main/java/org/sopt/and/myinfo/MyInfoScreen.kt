ae50a0f5ccfd39bf93315fad42c35661ab81212e
Jaemin <kamja990510@gmail.com>
2024-11-01 15:40:18 +0900

    [refactor] #5 Make MyInfoProfile component

--- app/src/main/java/org/sopt/and/myinfo/MyInfoScreen.kt
+++ app/src/main/java/org/sopt/and/myinfo/MyInfoScreen.kt
@@ -3,29 +3,21 @@ package org.sopt.and.myinfo
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.outlined.Notifications
-import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.and.R
 import org.sopt.and.components.EmptyInfoBox
+import org.sopt.and.myinfo.components.MyInfoProfile
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 import org.sopt.and.ui.theme.Black100
 import org.sopt.and.ui.theme.Grey100
@@ -73,52 +65,6 @@ fun MyInfoScreen(
             ),
             stringResource(R.string.my_program_of_interest_empty_text),
             modifier = Modifier.weight(0.3f)
-        )
-    }
-}
-
-@Composable
-fun MyInfoProfile(
-    myEmail: String,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(color = Grey100)
-            .padding(16.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            imageVector = Icons.Default.AccountCircle,
-            contentDescription = stringResource(id = R.string.my_account_icon_description),
-            modifier = Modifier.size(80.dp),
-            tint = White100
-        )
-
-        Spacer(modifier = Modifier.width(5.dp))
-
-        Text(
-            text = myEmail,
-            color = White100
-        )
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Icon(
-            imageVector = Icons.Outlined.Notifications,
-            contentDescription = stringResource(id = R.string.my_notification_icon_description),
-            modifier = Modifier.size(30.dp),
-            tint = White100
-        )
-
-        Spacer(modifier = Modifier.width(24.dp))
-
-        Icon(
-            imageVector = Icons.Outlined.Settings,
-            contentDescription = stringResource(id = R.string.my_setting_icon_description),
-            modifier = Modifier.size(30.dp),
-            tint = White100
         )
     }
 }

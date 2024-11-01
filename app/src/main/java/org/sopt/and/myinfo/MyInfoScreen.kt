0da54bf2ba4e7af584f10058b3c1b1ada329b541
Jaemin <kamja990510@gmail.com>
2024-11-01 15:43:34 +0900

    [refactor] #5 Make MyInfoPaymentInducementBox component

--- app/src/main/java/org/sopt/and/myinfo/MyInfoScreen.kt
+++ app/src/main/java/org/sopt/and/myinfo/MyInfoScreen.kt
@@ -5,11 +5,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -17,12 +15,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.and.R
 import org.sopt.and.components.EmptyInfoBox
+import org.sopt.and.myinfo.components.MyInfoPaymentInducementBox
 import org.sopt.and.myinfo.components.MyInfoProfile
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 import org.sopt.and.ui.theme.Black100
-import org.sopt.and.ui.theme.Grey100
-import org.sopt.and.ui.theme.Grey200
-import org.sopt.and.ui.theme.White100
 
 @Composable
 fun MyInfoScreen(
@@ -68,32 +64,6 @@ fun MyInfoScreen(
         )
     }
 }
-
-@Composable
-fun MyInfoPaymentInducementBox(
-    paymentInducementText: String,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(color = Grey100)
-            .padding(16.dp)
-    ) {
-        Text(
-            text = paymentInducementText,
-            color = Grey200
-        )
-
-        Spacer(modifier = Modifier.height(4.dp))
-
-        Text(
-            text = stringResource(id = R.string.my_to_payment_button),
-            color = White100
-        )
-    }
-}
-
 
 @Preview(
     showBackground = true,

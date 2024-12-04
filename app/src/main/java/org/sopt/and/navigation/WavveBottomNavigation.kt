2a8b5175f4a770d1c7c84902c39f18352d227b1e
kamja0510 <kamja990510@gmail.com>
2024-12-04 19:51:18 +0900

    [fix] #7 bottom navigation 프리뷰 보이도록 수정

--- app/src/main/java/org/sopt/and/navigation/WavveBottomNavigation.kt
+++ app/src/main/java/org/sopt/and/navigation/WavveBottomNavigation.kt
@@ -1,12 +1,18 @@
 package org.sopt.and.navigation
 
 import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -17,6 +23,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
+import org.sopt.and.R
 import org.sopt.and.ui.theme.Grey100
 import org.sopt.and.ui.theme.Grey200
 
@@ -75,10 +82,30 @@ fun WavveBottomNavigation(
 @Preview
 @Composable
 fun WavveBottomNavigationPreview() {
+    val index = remember{ mutableIntStateOf(0) }
     WavveBottomNavigation(
-        listOf(),
+        listOf(
+            WavveBottomNavigationItem(
+                label = R.string.bottom_navigation_home_label,
+                icon = Icons.Default.Home,
+                route = Routes.Home,
+                index = 0
+            ),
+            WavveBottomNavigationItem(
+                label = R.string.bottom_navigation_search_label,
+                icon = Icons.Default.Search,
+                route = Routes.Search,
+                index = 1
+            ),
+            WavveBottomNavigationItem(
+                label = R.string.bottom_navigation_my_info_label,
+                icon = Icons.Default.AccountCircle,
+                route = Routes.MyInfo,
+                index = 2
+            )
+        ),
         navController = rememberNavController(),
-        setNavigationSelectedScreenIndex = TODO(),
-        navigationSelectedScreenIndex = TODO(),
+        setNavigationSelectedScreenIndex = { index.intValue = it },
+        navigationSelectedScreenIndex = index.intValue,
     )
 }
